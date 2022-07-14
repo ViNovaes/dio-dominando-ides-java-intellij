220f5e31ebced73996f9229202495fb3d5db856d
ViNovaes <victoria.novaes07@hotmail.com>
2022-07-14 14:01:45 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+        System.out.println(cachorro);
+
+        /*ctrl apaga a linha toda*/
+        /*shift + f6 renomeia o programa*/
+        /*ctrl + d duplica a linha*/
+
+        /*int a = 5;
+        int b = 2;
+        System.out.println("Hello World! " + (a-b)); ctrl+shift+/ comenta  */
+    }
+}
